baf7734c02c6724d6739a0bbf91f21ed532a2cf4
anibal <anibalmoreno1998@gmail.com>
2025-12-24 10:47:37 +0000

    first commit

---
+++ src/main/java/com/universe/cosmos_management/CosmosManagementApplication.java
@@ -0,0 +1,13 @@
+package com.universe.cosmos_management;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CosmosManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CosmosManagementApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/universe/cosmos_management/CosmosManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.universe.cosmos_management;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CosmosManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
